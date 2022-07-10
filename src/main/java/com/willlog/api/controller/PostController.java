b37f8e71746bccd94fa8b038537c8248e4cfd197
freewill0819 <89326976+freewill0819@users.noreply.github.com>
2022-07-11 01:02:29 +0900

    볼로그 crud (데이터검증2)

--- src/main/java/com/willlog/api/controller/PostController.java
+++ src/main/java/com/willlog/api/controller/PostController.java
@@ -22,17 +22,7 @@ public class PostController {
     //POST Method
 
   @PostMapping("/posts")
-   public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result){
-      if(result.hasErrors()){
-          List<FieldError> fieldErrors = result.getFieldErrors();
-          FieldError firstFieldError = fieldErrors.get(0);
-          String fieldName = firstFieldError.getField();//title
-          String errorMessage = firstFieldError.getDefaultMessage();//에러메세지
-
-          Map<String, String> error = new HashMap<>();
-          error.put(fieldName, errorMessage);
-          return error;
-      }
+   public Map<String, String> post(@RequestBody @Valid PostCreate params){
        return Map.of();
    }
 }
--- src/test/java/com/willlog/api/controller/PostControllerTest.java
+++ src/test/java/com/willlog/api/controller/PostControllerTest.java
@@ -46,9 +46,11 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
                 )
-                .andExpect(status().isOk())
+                .andExpect(status().isBadRequest())
 //                .andExpect(content().string("Hello World"))
-                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
 
     }
